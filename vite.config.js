e7f6b7771258d0707f19aefef33073af081032ba
jaco266w <jaco266w@stud.kea.dk>
2023-10-28 12:53:47 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    target: 'es2015',
+    polyfillDynamicImport: false,
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         // Serve JavaScript files with the correct MIME type
+        format: 'es',
+        entryFileNames: '[name]-[hash].js',
+        chunkFileNames: '[name]-[hash].js'
+      },
+      // Serve JavaScript files with the correct MIME type
+      outputOptions: {
         mimeTypes: {
           js: 'application/javascript'
         }
