552a03e5936343903d8797e71d8c5190b0deb518
jaco266w <jaco266w@stud.kea.dk>
2023-10-28 13:03:39 +0200

    vite 2

--- vite.config.js
+++ vite.config.js
@@ -14,10 +14,28 @@ export default defineConfig({
         // Serve JavaScript files with the correct MIME type
         format: 'es',
         entryFileNames: '[name]-[hash].js',
-        chunkFileNames: '[name]-[hash].js',
-        // Add the following line to serve JavaScript files with the correct MIME type
-        dynamicImportFunction: '__import__'
+        chunkFileNames: '[name]-[hash].js'
       }
     }
+  },
+  // Use the renderDynamicImport plugin hook to serve JavaScript files with the correct MIME type
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+    plugins: [
+      {
+        name: 'renderDynamicImport',
+        async renderChunk(code, chunk) {
+          if (chunk.type === 'chunk') {
+            return {
+              code: code.replace(
+                /__import__\(/g,
+                'import.meta.ROLLUP_CHUNK_URL + '
+              )
+            }
+          }
+          return null
+        }
+      }
+    ]
   }
 })
