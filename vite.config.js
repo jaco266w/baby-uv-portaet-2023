4bb760632874bad19b30651d02fd936f25c8484b
jaco266w <jaco266w@stud.kea.dk>
2023-10-28 12:47:11 +0200

    vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Serve JavaScript files with the correct MIME type
+        mimeTypes: {
+          js: 'application/javascript'
+        }
+      }
+    }
+  }
 })
