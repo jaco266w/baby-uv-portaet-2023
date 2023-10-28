ddd119589f6bed3d1a996bb59f4be89aa573fcc6
jaco266w <jaco266w@stud.kea.dk>
2023-10-28 13:11:05 +0200

    vite config reset

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import replace from '@rollup/plugin-replace'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'es2015',
-    polyfillDynamicImport: false,
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        // Serve JavaScript files with the correct MIME type
-        format: 'es',
-        entryFileNames: '[name]-[hash].js',
-        chunkFileNames: '[name]-[hash].js'
-      }
-    }
-  },
-  // Use the @rollup/plugin-replace plugin to replace the __import__ function with the correct dynamic import function
-  optimizeDeps: {
-    include: ['react', 'react-dom'],
-    plugins: [
-      replace({
-        __import__: 'import.meta.ROLLUP_CHUNK_URL + '
-      })
-    ]
-  }
 })
