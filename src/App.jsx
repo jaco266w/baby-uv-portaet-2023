12002662421d60caaf7fef5e22c8d54fdf1108b2
jaco266w <jaco266w@stud.kea.dk>
2023-11-19 21:04:39 +0100

    theme-color

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import Info from "./components/Info";
 
 export default function App() {
   return (
-    <>
+    <main>
       <Hero />
       <Info />
       <SignupForm />
-    </>
+    </main>
     
   );
 }
